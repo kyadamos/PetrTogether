2b94deab0d195f81a9421eda0bc75f1c609bfee7
Shashwat Sparsh <shashwat.sparsh@gmail.com>
2021-05-13 15:21:34 -0700

    Added Loops for Data Storage
    Automated the max count assignment and the project creation assignment

--- Saucy Script/saucyScript.py
+++ Saucy Script/saucyScript.py
@@ -19,6 +19,10 @@ https://stackoverflow.com/questions/28218698/how-to-iterate-over-columns-of-pand
 """
     
 import saucyScriptClasses
+import numpy as np;
+#import tinkter as tk;
+import pandas as pd;
+
 #import random
 
 #Primary Sorting Function
@@ -45,37 +49,38 @@ def preferenceConverter(preference):
 
 #End
 
-import numpy as np;
-#import tinkter as tk;
-import pandas as pd;
-
 numProjects = 15;
 projectList = list();
 studentList = list();
 projectPreferences = list();
 
-for currentProjectNum in range(1,16):    
-    #print(currentProjectNum);
-    projectList.append(saucyScriptClasses.Project(currentProjectNum, 6));
-
-    
-data = pd.read_csv (r'D:\Documents\GitHub\PetrTogether\Saucy Script\F20_dataModified.csv')   
+#Read Student Data  
+data = pd.read_csv (r'.\F20_Test.csv')   
 df = pd.DataFrame(data, columns= ['Student number','Preference 1', 'Preference 2', 
                                   'Preference 3', 'Preference 4', 'Preference 5', 
                                   'Mechanical Skills', 'Electronics', 'Welding',
                                   'Metal Machining', 'Woodworking', 'Power Tools',
                                   '3D Printing', 'Laser Cutting']);
-
 #print(df);
+
+#Read Project MaxCount Data
+maxCountData = pd.read_csv(r'.\Team_size_constraints.csv')
+maxCountDF = pd.DataFrame(maxCountData, columns = ['Project Number','F20','W21'])
+#print(maxCountDF)
+for currentProjectNum, row in maxCountDF.iterrows():
+    #print(row.values[1])
+    currentProject = saucyScriptClasses.Project(currentProjectNum + 1, row.values[1]);
+    projectList.append(currentProject)
+
+#print(projectList[0].getMaxCount())
 
 
 numStudents = len(df) - 1;
-
 #Create Student List
 for i, row in df.iterrows():
     #Create Student Object
-    student = row.values[0];
-    currentStudent = saucyScriptClasses.Student(student);
+    student = row.values[0]
+    currentStudent = saucyScriptClasses.Student(student)
     #Pull Relevant data for Preferences and Skills
     projectPreferences = (preferenceConverter(row.values[1]), preferenceConverter(row.values[2]),
                           preferenceConverter(row.values[3]), preferenceConverter(row.values[4]),
@@ -83,69 +88,47 @@ for i, row in df.iterrows():
     skills = (row.values[6], row.values[7], row.values[8], row.values[9],
               row.values[10], row.values[11], 3, row.values[13]);
     
-    #row.values[12]
     #Set Student Preferences and Skills
-    currentStudent.setPreferences(projectPreferences);
-    currentStudent.setSkills(skills);
+    currentStudent.setPreferences(projectPreferences)
+    currentStudent.setSkills(skills)
     #Add Student to list
-    studentList.append(currentStudent);
+    studentList.append(currentStudent)
 
 #Sort into Primary Preferences    
 for currentStudentNum in range(len(df)):
-    currentStudent = studentList[currentStudentNum];
-    preferences = currentStudent.getPreferences();
-    #print(preferences[0]);
-    proj = projectList[preferences[0] - 1];
-    proj.addStudent(currentStudent);
+    currentStudent = studentList[currentStudentNum]
+    preferences = currentStudent.getPreferences()
+    #print(preferences[0])
+    proj = projectList[preferences[0] - 1]
+    proj.addStudent(currentStudent)
     #print(proj);
     #if currentStudentNum == 0:
         #print(preferences);
 
-projectList[0].setAffinityCoefficients([.15, .15, .05, .05, .15, .15, .15, .15]); #UAV Forge
-projectList[1].setAffinityCoefficients([.05, .05, .05, .1, .2, .15, .2, .2]);
-projectList[2].setAffinityCoefficients([.1, .1, .05, .05, .1, .1, .1, .1]);
-projectList[3].setAffinityCoefficients([.1, .1, .05, .05, .1, .1, .1, .1]);
-projectList[4].setAffinityCoefficients([.1, .1, .05, .05, .1, .1, .1, .1]);
-projectList[5].setAffinityCoefficients([.1, .1, .05, .05, .1, .1, .1, .1]);
-projectList[6].setAffinityCoefficients([.1, .1, .05, .05, .1, .1, .1, .1]);
-projectList[8].setAffinityCoefficients([.1, .1, .05, .05, .1, .1, .1, .1]);
-projectList[9].setAffinityCoefficients([.1, .1, .05, .05, .1, .1, .1, .1]);
-projectList[10].setAffinityCoefficients([.1, .1, .05, .05, .1, .1, .1, .1]);
-projectList[11].setAffinityCoefficients([.1, .1, .05, .05, .1, .1, .1, .1]);
-projectList[12].setAffinityCoefficients([.1, .1, .05, .05, .1, .1, .1, .1]);
-projectList[13].setAffinityCoefficients([.1, .1, .05, .05, .1, .1, .1, .1]);
-projectList[14].setAffinityCoefficients([.1, .1, .05, .05, .1, .1, .1, .1]);
+#Use Numpy random to populate coefficients
+coeffSum = 10
+numCoeffs = 8
+for i in range(len(projectList)):
+    randCoeffArray = (np.random.multinomial(coeffSum, np.ones(numCoeffs)/numCoeffs, size=1)[0]) / 10
+    projectList[i].setAffinityCoefficients(randCoeffArray)
+    
+#print(projectList[0].getAffinityCoefficients())
+#print(studentList[0].getSkills());
+#print(len(projectList[11].getRoster()))
 
-#Automate this by importing as df
-projectList[0].setMaxCount(6);
-projectList[1].setMaxCount(5);
-projectList[2].setMaxCount(5);
-projectList[3].setMaxCount(5);
-projectList[4].setMaxCount(5);
-projectList[5].setMaxCount(4);
-projectList[6].setMaxCount(5);
-projectList[7].setMaxCount(5);
-projectList[8].setMaxCount(4);
-projectList[9].setMaxCount(4);
-projectList[10].setMaxCount(4);
-projectList[11].setMaxCount(4);
-projectList[12].setMaxCount(6);
-projectList[13].setMaxCount(5);
-projectList[14].setMaxCount(4);
-
-#'''
+'''
 for i in range(len(projectList)):
     currentProject = projectList[i];
     currentAffinityCoefficients = currentProject.getAffinityCoefficients();
     currentRoster = currentProject.getRoster();
-    for j in range(len(currentRoster)):
-        currentStudent = currentRoster[j];
-        currentSkillSet = currentStudent.getSkills();
-        currentAffinityScore = sum(np.multiply(currentAffinityCoefficients, currentSkillSet));
-        currentStudent.setAffinityScore(currentAffinityScore());
-    #print(currentRoster);
-    #break
-
+    if len(currentRoster) != 0:
+        for j in range(len(currentRoster)):
+            currentStudent = currentRoster[j];
+            currentSkillSet = currentStudent.getSkills();
+            print(currentSkillSet)
+            print(currentAffinityCoefficients)
+            currentAffinityScore = sum(np.multiply(currentAffinityCoefficients, currentSkillSet));
+            currentStudent.setAffinityScore(currentAffinityScore());
 #'''
 
 #Project Student Table = pd.DataFrame
@@ -154,19 +137,19 @@ for i in range(len(projectList)):
 #proj = projectList[0];
 #print(proj);
 
-'''
-skillset = (studentList[0]).getSkills();
-print(skillset);
-currentProject = projectList[0];
+#'''
+#skillset = (studentList[0]).getSkills();
+#print(skillset);
+currentProject = projectList[16];
 currentAffinityCoefficients = currentProject.getAffinityCoefficients();
 currentRoster = currentProject.getRoster();
-currentStudent = currentRoster[0];
+currentStudent = currentRoster[1];
 currentSkillSet = currentStudent.getSkills();
 currentAffinityScore = sum(np.multiply(currentAffinityCoefficients, currentSkillSet));
 #print(currentAffinityScore);
 currentStudent.setAffinityScore(currentAffinityScore);
 print(currentStudent.getCurrentAffinityScore());
-'''
+#'''
 
 
 ''' Framework For Automated Assignment and Comparison
